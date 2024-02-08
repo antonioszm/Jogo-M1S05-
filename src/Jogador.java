a2023ae70734e83e88cc8efb86b31d896797ddef
antonioszm <75432982+antonioszm@users.noreply.github.com>
2024-02-08 19:29:06 -0300

    Exercicio2

--- src/Jogador.java
+++ src/Jogador.java
@@ -1,9 +1,48 @@
 public class Jogador {
 
-    public static String nome;
-    public static int idade;
-    public static int pontuacao;
-    public static int numeroTentativas;
+    private static String nome;
+    private  static int idade;
+    private static int pontuacao;
+    private static int numeroTentativas;
+
+    public Jogador(String nome, int idade, int pontuacao, int numeroTentativas) {
+        this.nome = nome;
+        this.idade = idade;
+        this.pontuacao = pontuacao;
+        this.numeroTentativas = numeroTentativas;
+    }
+
+    public static String getNome() {
+        return nome;
+    }
+
+    public static int getIdade() {
+        return idade;
+    }
+
+    public static int getPontuacao() {
+        return pontuacao;
+    }
+
+    public static int getNumeroTentativas() {
+        return numeroTentativas;
+    }
+
+    public static void setNome(String nome) {
+        Jogador.nome = nome;
+    }
+
+    public static void setIdade(int idade) {
+        Jogador.idade = idade;
+    }
+
+    public static void setPontuacao(int pontuacao) {
+        Jogador.pontuacao = pontuacao;
+    }
+
+    public static void setNumeroTentativas(int numeroTentativas) {
+        Jogador.numeroTentativas = numeroTentativas;
+    }
 
     public static void addPontos(int pontos){
         pontuacao += pontos;
