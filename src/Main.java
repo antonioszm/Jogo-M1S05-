25bbc514212b7943e64b7913556203bae1c8dc77
antonioszm <75432982+antonioszm@users.noreply.github.com>
2024-02-10 11:12:40 -0300

    Exercicio5

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,36 @@
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static List<Jogador> Jogadores = new ArrayList<>();
 
     public static void main(String[] args) {
+        while (true){
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Digite seu nome!: ");
+            String nome = scanner.next();
+            System.out.print("Digite sua idade!: ");
+            int idade = scanner.nextInt();
+            Jogador jogador3 = new Jogador("antonio", 2, 0,0);
+            Jogadores.add(jogador3);
 
-        Jogador jogador = new Jogador("antonio", 16, 0,0);
 
-        Jogadores.add(jogador);
+            for (Jogador jogador1 : Jogadores){
+                if (nome.equals(jogador1.getNome())){ // if para checar se o nome ja está cadastrado
+                    System.out.println("Esse nome já está em uso. Tente outro!");
+                    System.out.print("Novo nome: ");
+                }  //se n for ele cria o jogador
 
-        Jogadores.sort(Comparator.comparingInt(Jogador::getPontuacao).reversed());
-
+                if (!nome.equals(jogador1.getNome())){
+                    Jogador jogador = new Jogador(nome, idade, 0,0);
+                    Jogadores.add(jogador);
+                    Jogadores.sort(Comparator.comparingInt(Jogador::getPontuacao).reversed());
+                    break;
+                }
+            }
+        }
     }
 
     public static void exibirTop10(){
@@ -20,7 +38,7 @@ public class Main {
         for (Jogador jogador : Jogadores){
             System.out.println("Jogador: "+ jogador + " - " + posicao);
             posicao++;
-            if (posicao == 11){
+            if (posicao == 11){ // termina o for so com 10
                 break;
             }
         }
