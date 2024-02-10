dd5811f134847778acef57d2f731652ec92e8b76
antonioszm <75432982+antonioszm@users.noreply.github.com>
2024-02-10 17:10:18 -0300

    Exercicio7 e muita coisa

--- src/Jogo.java
+++ src/Jogo.java
@@ -5,13 +5,13 @@ public class Jogo {
     private static Jogador melhorJogador;
     private static int numeroJogadas;
 
-    public static void jogar(Jogador jogador){
+    public static void jogar(Jogador jogador) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Escolha entre, Pedra, Papel e Tesoura!");
         String resposta = scanner.next();
         Random random = new Random();
         int escolhaDaMaquina = random.nextInt(1, 3);
-        switch (escolhaDaMaquina){
+        switch (escolhaDaMaquina) {
             case 1:
                 System.out.println("Você ganhou!!!!!");
                 jogador.addPontos(1);
@@ -29,6 +29,37 @@ public class Jogo {
                 jogador.adicionarTentativas(1);
                 numeroJogadas++;
                 break;
+        }
+    }
+
+    public static void jogar(Jogador jogador, int num) {
+        Scanner scanner = new Scanner(System.in);
+        int numero = new Random().nextInt(0, num);
+        int numeroDigitado;
+        do {
+
+            System.out.print("Tente adivinhar: ");
+            numeroDigitado = scanner.nextInt();
+            if (numeroDigitado > numero) {
+                System.out.println("O numero é menor");
+            } else if (numeroDigitado < numero) {
+                System.out.println("numero é maior");
+            }
+        } while (numero != numeroDigitado);
+        System.out.println("Errou");
+        int tentativasRestantes = 10;
+
+        while (tentativasRestantes > 0) {
+            System.out.println("Tentativas restantes: " + tentativasRestantes);
+
+            boolean acertou = false;
+            if (numeroDigitado == numero) {
+                acertou = true;
+            }
+            if (!acertou) {
+                tentativasRestantes--;
+                System.out.println("Tentativas restantes: " + tentativasRestantes);
+            }
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,60 @@ public class Main {
     public static List<Jogador> Jogadores = new ArrayList<>();
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("====================================================");
+        System.out.println("|              O que Deseja Fazer?                 |");
+        System.out.println("|--------------------------------------------------|");
+        System.out.println("|  (1) Criar Jogador                               |");
+        System.out.println("|  (2) Jogar                                       |");
+        System.out.println("|  (3) Exibir Top 10                               |");
+        System.out.println("====================================================");
+        System.out.print("Escolha uma opção: ");
+
+        int resposta = scanner.nextInt();
+
+        switch (resposta){
+            case 1:
+                criarJogador();
+                break;
+            case 2:
+                System.out.println("===========================================");
+                System.out.println("|              Quer exibir:               |");
+                System.out.println("|-----------------------------------------|");
+                System.out.println("| (1) Pedra Papel e Tesoura               |");
+                System.out.println("| (2) Adivinhe o numero                   |");
+                System.out.println("===========================================");
+                System.out.print("Escolha uma opção: ");
+                int resposta2 = scanner.nextInt();
+                if (resposta2 == 1){
+                    Jogo.jogar(criarJogador());
+                } else {
+                    System.out.println("Digite o numero limite!");
+                    int num = scanner.nextInt();
+                    Jogo.jogar(criarJogador(), num);
+                }
+                break;
+
+            case 3:
+                exibirTop10();
+                break;
+
+        }
+
+
+    }
+
+    public static void exibirTop10(){
+        int posicao = 1;
+        for (Jogador jogador : Jogadores){
+            System.out.println("Jogador: "+ jogador + " - " + posicao);
+            posicao++;
+            if (posicao == 11){ // termina o for so com 10
+                break;
+            }
+        }
+    }
+    public static Jogador criarJogador(){
         while (true){
             Scanner scanner = new Scanner(System.in);
             System.out.print("Digite seu nome!: ");
@@ -27,19 +81,8 @@ public class Main {
                     Jogador jogador = new Jogador(nome, idade, 0,0);
                     Jogadores.add(jogador);
                     Jogadores.sort(Comparator.comparingInt(Jogador::getPontuacao).reversed());
-                    break;
+                    return jogador;
                 }
-            }
-        }
-    }
-
-    public static void exibirTop10(){
-        int posicao = 1;
-        for (Jogador jogador : Jogadores){
-            System.out.println("Jogador: "+ jogador + " - " + posicao);
-            posicao++;
-            if (posicao == 11){ // termina o for so com 10
-                break;
             }
         }
     }
